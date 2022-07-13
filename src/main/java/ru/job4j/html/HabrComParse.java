d57878f7fc0163a5a5625269015f6eb9716897c7
EfmovMaxim <efimov-m-v@ya.ru>
2022-07-13 19:10:12 +0300

    2.1.1. Парсинг

--- src/main/java/ru/job4j/html/HabrComParse.java
+++ src/main/java/ru/job4j/html/HabrComParse.java
@@ -10,26 +10,36 @@ import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TimeZone;
 
 public class HabrComParse {
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://career.habr.com/vacancies/java_developer").get();
-        Elements row = doc.select(".vacancy-card__inner");
+        List<String> urlList = new ArrayList<>();
+        urlList.add("https://career.habr.com/vacancies/java_developer");
+
+        for (int i = 2; i < 6; i++) {
+            urlList.add(urlList.get(0) + "?page=" + i);
+        }
+
+        for (String url : urlList) {
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".vacancy-card__inner");
 
 
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
 
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+            for (Element td : row) {
+                Element href = td.child(1);
+                Element date = td.child(0).child(0);
+                Element text = td.child(2).child(1).child(0);
 
-        for (Element td : row) {
-            Element href = td.child(1);
-            Element date = td.child(0).child(0);
-            Element text = td.child(2).child(1).child(0);
-
-            System.out.println(dateFormat.parse(date.attr("datetime")));
-            System.out.println(href.attr("abs:href"));
-            System.out.println(text.text());
+                System.out.println(dateFormat.parse(date.attr("datetime")));
+                System.out.println(href.attr("abs:href"));
+                System.out.println(text.text());
+            }
         }
     }
 }
